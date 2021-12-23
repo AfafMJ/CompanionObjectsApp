0889257abbe321e10e50eb1de4f177d938eece81
AfafMJ <10.afafalharbi@gmail.com>
2021-12-23 17:17:33 +0300

    lottie library

--- app/src/main/java/com/afaf/companionobjectsapp/MainActivity.kt
+++ app/src/main/java/com/afaf/companionobjectsapp/MainActivity.kt
@@ -6,22 +6,45 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var constraintLayout: ConstraintLayout
     lateinit var dayNightEditText: EditText
     lateinit var changeButton: Button
+    var isChecked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         constraintLayout = findViewById(R.id.clbackground)
         dayNightEditText = findViewById(R.id.etDayNight)
-        changeButton = findViewById(R.id.btChange)
-        changeButton.setOnClickListener {
-            val Backgrounds = Background(this)
-            Backgrounds.set(constraintLayout, dayNightEditText.text.toString())
+        var check = findViewById<LottieAnimationView>(R.id.switchDay)
+        check.setOnClickListener {
+            if (isChecked){
+                check.setMinAndMaxProgress(0.5f,1.0f)
+                check.playAnimation()
+
+                val Backgrounds = Background(this)
+                Backgrounds.set(constraintLayout, dayNightEditText.text.toString())
+                isChecked = false
+
+            } else {
+                check.setMinAndMaxProgress(0.0f,0.1f)
+                check.playAnimation()
+                val Backgrounds = Background(this)
+                Backgrounds.set(constraintLayout, dayNightEditText.text.toString())
+
+                isChecked = true
+
+
+            }
         }
+
+//        changeButton.setOnClickListener {
+//            val Backgrounds = Background(this)
+//            Backgrounds.set(constraintLayout, dayNightEditText.text.toString())
+//        }
     }
 }
